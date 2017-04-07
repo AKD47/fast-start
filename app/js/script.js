2e42da585fe77863bf3feccd402d25fce6e962bd
dmitryi <dmitryi.zavadskyi@yandex.ru>
2017-04-07 12:00:06 +0300

    add scrollBlock script

--- app/js/script.js
+++ app/js/script.js
@@ -132,4 +132,30 @@ $(document).ready(function () {
         });
     }
     /*top-form validation*/
+
+    /*scroll block*/
+    var element = $('#sidebar');//елемент, который необходимо сделать фиксированным
+    if (element.length > 0) {//проверка наличия элемента на странице
+        var elementPosition = element.offset().top;//определяем позицию элемента относительно верха документа
+        $(window).scroll(function () {//при прокрутке окна браузера
+            fixedScroll(element, elementPosition, $('#footer'));//вызывается функция с заданными селекторами
+        });
+    }
+
+    /*close scroll block*/
 });
+
+function fixedScroll(element, elementPosition, blockElement) {//функция фиксированногоблока, с селекторами элемента, его позиционирования и преграждающего блока
+    var top = $(document).scrollTop(),//значение отступа прокрутки сверху для первого элемента
+        blockingElement = blockElement.offset().top,//позиция блокирующего блока отностительно верха документа
+        height = element.outerHeight();//высота элемента, включающая внутренние и внешние отступы
+    if (top > elementPosition && top < blockingElement - height) {//
+        element.addClass('fixed').removeAttr("style");
+    }
+    else if (top > blockingElement - height) {
+        element.removeClass('fixed').css({'position': 'absolute', 'bottom': '50px', 'right': '0'});
+    }
+    else {
+        element.removeClass('fixed');
+    }
+}
