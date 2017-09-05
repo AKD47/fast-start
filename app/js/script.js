380f28b7c591343b2f9a56c2bafb9b8b09007654
AKD47 <van83horn@gmail.com>
2017-09-05 15:03:41 +0300

    script fix

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,37 @@
+if ($('#map').length > 0) {
+    /*yandex map*/
+    var map = new Map();
+    map.init({
+        selector: '#map2',
+        center: 'г. Донецк, ул. Артема 75',
+        zoom: 12,
+        placemarks: [
+            {
+                address: 'г. Донецк, ул. Артема 75',
+                options: [
+                    {key: 'draggable', value: true}
+                ],
+                properties: [
+                    {key: 'hintContent', value: 'Тыц'},
+                    {key: 'balloonContentHeader', value: "Предприятия Донецка"},
+                    {key: 'balloonContentBody', value: "<h1>ArtCraft</h1>"}
+                ]
+            },
+            {
+                address: 'г. Донецк, ул. Артема 100',
+                options: [
+                    {key: 'draggable', value: true}
+                ],
+                properties: [
+                    {key: 'hintContent', value: 'Пока'}
+                ]
+            }
+        ]
+    });
+    /*close yandex map*/
+}
+
+
 $(document).ready(function () {
 
     /*header mobile menu*/
@@ -46,38 +80,7 @@ $(document).ready(function () {
             });
         }
     });
-    /*close block animation*/
-
-    /*yandex map*/
-    var map = new Map();
-    map.init({
-        selector: '#map2',
-        center: 'г. Донецк, ул. Артема 75',
-        zoom: 12,
-        placemarks: [
-            {
-                address: 'г. Донецк, ул. Артема 75',
-                options: [
-                    {key: 'draggable', value: true}
-                ],
-                properties: [
-                    {key: 'hintContent', value: 'Тыц'},
-                    {key: 'balloonContentHeader', value: "Предприятия Донецка"},
-                    {key: 'balloonContentBody', value: "<h1>ArtCraft</h1>"}
-                ]
-            },
-            {
-                address: 'г. Донецк, ул. Артема 100',
-                options: [
-                    {key: 'draggable', value: true}
-                ],
-                properties: [
-                    {key: 'hintContent', value: 'Пока'}
-                ]
-            }
-        ]
-    });
-    /*close yandex map*/
+    /*close block animation*/   
 
     /*top-form validation*/
     if (document.getElementById('top-form')) {
