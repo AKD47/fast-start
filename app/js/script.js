b2db9009fe7f327373c50a43e33012ff1f739221
AKD47 <van83horn@gmail.com>
2018-06-11 16:49:03 +0300

    password indicator script

--- app/js/script.js
+++ app/js/script.js
@@ -360,6 +360,63 @@ $(document).ready(function () {
         }
     });
     /*close password*/
+    /*password*/
+    $(document).on('input', '.top__form--field.second', function (event) {//функция ввода текста в поле "пароль"
+        var value = $(this).val();//получаем значение поля
+        var indicator = $(this).next('.pass-value');//находим блок с индикатором, рассположенный за полем
+        var value_label = indicator.find('.pass-value__title');//находим заголовок индикатора
+        var confirm = $(this).closest('.top__form--fields-wrapper').find('.top__form--field.third');//находим поле для проверки пароля
+
+        if (value.length != 0) {//при заполнении поля пароля
+            confirm.attr('disabled', false);//убираем у поля проверки запрет на ввод
+        } else {//при пустом поле пароля
+            confirm.attr('disabled', true);//добавляем запрет на ввод
+            confirm.val('');//задаем полю пустое значение
+            confirm.next('.pass-value').hide().removeClass('strong');//прячем блок и убираем у него класс strong
+        }
+
+        if (value.length < 8 && value.match(/[a-zA-Z0-9]/)) {//при проверке ввода в поле менне 8 символов с обязательным наличием больших латинских букв и цифр
+            $(this).removeClass('valid').addClass('invalid');//полю добавляется класс invalid
+        } else {// если услови выполнено
+            $(this).removeClass('invalid').addClass('valid');//полю добавляется класс valid
+        }
+        if ($(this).hasClass('invalid')) {//при наличии у поля класса invalid
+            indicator.show().removeClass('strong');//показывается блок индикатора и ему убирается класс strong
+            value_label.text('Heslo je středně silné');//заголовку добовляется нужная надпись
+        } else if ($(this).hasClass('valid')) {//при наличии у поля класса valid
+            indicator.addClass('strong');//показывается блок индикатора и ему добавляется класс strong
+            value_label.text('Heslo je silné');//заголовку добовляется нужная надпись
+        }
+        if (value.length < 1) {//если поле не заполнено
+            indicator.hide().removeClass('strong');//блок индикатора скрывается
+            value_label.text('Heslo je středně silné');//заголовок ставиться по умолчанию
+        }
+    });
+
+    $(document).on('input', '.top__form--field.third', function (event) {//функция проверки повтора паролля
+        var value = $(this).val();//получаем значение поля
+        var main_passw = $(this).closest('.top__form--fields-wrapper').find('.top__form--field.second');//находим поле ранее введеного пароля
+        var indicator = $(this).next('.pass-value');//находим блок с индикатором, рассположенный за полем
+        var value_label = indicator.find('.pass-value__title');//находим заголовок индикатора
+
+        if (value == main_passw.val()) {//при совпадении значения обоих полей
+            $(this).removeClass('invalid').addClass('valid');//полю проверки добавляется класс valid
+        } else {// если услови не выполнено
+            $(this).removeClass('valid').addClass('invalid');//полю проверки добавляется класс invalid
+        }
+        if ($(this).hasClass('invalid')) {//при проверке наличия у поля проверки класса invalid
+            indicator.show().removeClass('strong');//показывается индикатор и ему удаляется класс strong
+            value_label.text('Heslo neodpovídá');//заголовку добовляется нужная надпись
+        } else if ($(this).hasClass('valid')) {//при проверке наличия у поля проверки класса valid
+            indicator.addClass('strong');//индикатору добавляется класс strong
+            value_label.text('Hesla se shodují');//заголовку добовляется нужная надпись
+        }
+        if (value.length < 1) {//если поле не заполнено
+            indicator.hide().removeClass('strong');//индикатор скрывается и у него убирается класс strong
+            value_label.text('Heslo neodpovídá');//заголовок ставиться по умолчанию
+        }
+    });
+    /*close password*/
 
 
     /*modal*/
